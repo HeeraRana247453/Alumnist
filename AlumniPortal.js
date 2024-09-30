001a03803bd1cdde7d195e27a4dcd442bf38db6d
Heera Rana <138364612+HeeraRana247453@users.noreply.github.com>
2024-09-30 19:04:31 +0530

    Add files via upload

---
+++ AlumniPortal.js
@@ -0,0 +1,9 @@
+// Toggle Sidebar for smaller screens
+const sidebarToggle = document.getElementById('sidebarToggle');
+const sidebar = document.querySelector('.sidebar');
+const mainContent = document.querySelector('.main-content');
+
+sidebarToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('collapsed');
+  mainContent.classList.toggle('expanded');
+});
